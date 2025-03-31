012d691df50c101b8fe7facaf8fdd75d23bb1850
legacybrand23 <codyleonlegacy@gmail.com>
2025-03-31 16:36:15 -0500

    Configuración final con rewrites para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',  // Asegúrate de que la base sea relativa
-  plugins: [
-    react() // Agrega el plugin para React
-  ]
-});
+  plugins: [react()],
+  base: './', // <-- esto está bien si tu index.html está en la raíz
+})
