a8913b6b637eb78d21536f889792c0d01e19025f
legacybrand23 <codyleonlegacy@gmail.com>
2025-03-31 15:37:37 -0500

    Agregado archivo vercel.json para despliegue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',  // Asegúrate de que esté './' sin barra al final.
-  plugins: [react()]
+  base: './',  // Asegúrate de que la base sea relativa
+  plugins: [
+    react() // Agrega el plugin para React
+  ]
 });
